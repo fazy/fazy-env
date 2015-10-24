cc9a4675760841eff3e278976ca4b4085a169fa5
Lars <lars@fazy.net>
2015-10-24 20:09:35 +0100

    Add PIP and Docker Compose

--- mint-desktop/setup.sh
+++ mint-desktop/setup.sh
@@ -34,6 +34,10 @@ END_OF_FILE
 
 chmod u+x ~/.bashrc
 
+# Install Python PIP (will be needed for a couple of things)
+sudo apt-get -yq install python-pip
+
+# Install Docker
 sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 foundRepo=$(grep apt.dockerproject.org /etc/apt/sources.list.d/additional-repositories.list)
 if [ -z "$foundRepo" ]; then
@@ -41,6 +45,8 @@ if [ -z "$foundRepo" ]; then
 fi
 
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt-get -yq install docker-engine
+sudo pip install docker-compose
 
 sudo gpasswd -a `whoami` docker
+
