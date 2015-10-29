ff9e66079b391cfed2a8798402950a6ccc5c9679
Lars Janssen <lars@fazy.net>
2015-10-29 17:27:50 +0000

    Add separate file for installing Node.js

---
+++ mint-desktop/node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Node and NPM
+
+# https://nodesource.com/blog/nodejs-v012-iojs-and-the-nodesource-linux-repositories
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+sudo apt-get install -y nodejs
+
