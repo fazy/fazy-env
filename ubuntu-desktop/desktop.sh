168c5a82a0388e2e14d21741e7af3d2b4b4fbeb9
Lars Janssen <lars@fazy.net>
2016-04-30 10:53:35 +0100

    Move Ubuntu menus back into windows

--- ubuntu-desktop/desktop.sh
+++ ubuntu-desktop/desktop.sh
@@ -5,3 +5,6 @@ gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 
 # Set theme
 gsettings set org.gnome.desktop.interface gtk-theme "Radiance"
+
+# Bring menus back into the window
+gsettings set com.canonical.Unity integrated-menus true
